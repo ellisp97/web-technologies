2cb498b259e28234c13b1bcabd9db0e7ac90d9fe
Shanik <id15663@my.bristol.ac.uk>
2018-05-12 15:49:57 +0100

    fixing

---
+++ routes/carousel.js
@@ -0,0 +1,70 @@
+"use strict";
+
+function setup(){
+  loadJSON('/all',gotData);
+  console.log('running');
+
+}
+
+function gotData(data){
+  console.log(data);
+  var keys = Object.keys(data);
+  console.log(keys);
+  for(var i=0; i<keys.length; i++){
+    var product = keys[i];
+    var price = data[product];
+    var x = random(width);
+    var y = random(height);
+    fill(255);
+    textSize(64);
+    text(product,x,y);
+  }
+}
+
+/*
+The below function makes the carousel work
+*/
+function carousel() {
+
+  /*
+  Prevents the bulk of the script from running if the parts
+  that we rely on are not present
+  if (!document.querySelector || !('classList' in document.body)) {
+    return false;
+  }*/
+
+  //These variables pick out the HTML elements that we need
+  //Namely the carousel container, next and previous buttons, and list items
+  var container = document.querySelector('.carousel-container');
+  var next = container.querySelector('.next');
+  var prev = container.querySelector('.prev');
+  var items = container.querySelectorAll('.carousel-content li');
+
+  var counter = 0; //a counter to note which element we are on
+  var amount = items.length; //the number of elements in the carousel
+  var current = items[0]; //the currently-displaying carousel element, initialised at the first list element
+
+
+
+  container.classList.add('active'); //make the container active
+
+  /*
+  The below function moves the 'current' class to the next or previous element
+  (or does nothing at all if direction = 0)
+  */
+  function navigate(direction) {
+    current.classList.remove('current'); //remove the current class from the current element
+    counter = (counter + direction + amount) % amount; //move forward or backward (modulo for wraparound)
+    current = items[counter]; //update the current variable to be the next or previous item
+    current.classList.add('current'); //add the current class to this element
+  }
+
+  next.addEventListener('click', function(ev) { //listen for a click event on the next button
+    navigate(1); //navigate to the next element in the carousel
+  });
+
+  prev.addEventListener('click', function(ev) {
+    navigate(-1); //navigate to the previous element in the carousel
+  });
+  navigate(0); //show the first element
+};
