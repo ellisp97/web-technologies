53d9d8cd57f8f79b8a9ff3866600b8d28318e7ac
Shanik <id15663@my.bristol.ac.uk>
2018-05-16 18:36:24 +0100

    async the data to send to client to make the graph, need to figure out how to send and render at the same time

--- site/routes/index.js
+++ site/routes/index.js
@@ -4,6 +4,7 @@ var bcrypt = require('bcrypt');
 var passport = require('passport');
 var cheerio = require('cheerio');
 var request = require('request');
+var util = require('util');
 
 
 const saltRounds = 10;
@@ -67,21 +68,90 @@ router.post('/login', passport.authenticate('local', {
 
 
 
-router.get('/profile', authenticationMiddleware(),
-  function(req, res, next) {
-    console.log(req.session);
+router.get('/profile', authenticationMiddleware(), function(req, res, next) {
+  console.log(req.session);
   var userid = req.user.user_id;
-
   console.log(req.user.user_id);
   console.log(userid);
-  db.all(`SELECT username, email FROM userData WHERE user_id =?`, [userid], function(err,results,fields){
-    if(err){reject(err);}
 
-    var username = results[0].username;
-    var email = results[0].email;
-    res.render('profile', { title: 'YOUR PROFILE', userid: userid, username: username, email:email});
+  var get_user_data = function(userid, cb) {
+    var query = `SELECT username, email, watched_product_ids FROM userData WHERE user_id =?`;
+    db.get(query, [userid], function(err, row) {
+      if(err){
+        console.error(err);
+        cb("error", null);
+      } else{
+        var user_data = {userid:userid, username:row.username, email:row.email, watched_ids:row.watched_product_ids};
+        cb(null, user_data);
+      }
+    });
+  };
 
-  });
+  var get_pd_array = function(id, cb){
+    var query = `SELECT prod_name, prod_price_history, date_update_history FROM productData where prod_id =?`;
+    db.get(query, [id], function(err, row) {
+      console.log(row);
+      if(err){
+        console.error(err);
+        cb("error", null);
+      }else{
+        cb(null, row);
+      }
+    });
+  };
+
+
+
+  var get_product_data_async = async function(prod_ids, cb){
+    var product_data_array = [];
+    var watched_id_string = prod_ids;
+    var watched_ids = watched_id_string.split(",");
+    for (id of watched_ids){
+      var pd_row = await get_pd_array_async(id);
+      product_data_array.push(pd_row);
+    };
+    console.log(product_data_array);
+    return product_data_array;
+  };
+
+  const get_user_data_async = util.promisify(get_user_data);
+  const get_pd_array_async = util.promisify(get_pd_array);
+
+  (async() => {
+    let user_data, product_data_array;
+    try {
+      user_data = await get_user_data_async(userid);
+      product_data_array = await get_product_data_async(user_data.watched_ids);
+    }
+    catch (err) {
+      return console.error(err);
+    }
+    // return res.send(product_data_array);
+    return res.render('profile', {title: 'YOUR PROFILE', userid:user_data.userid, username:user_data.username, email:user_data.email});
+  })();
+
+  // db.get(`SELECT username, email, watched_product_ids FROM userData WHERE user_id =?`, [userid], function(err,result) {
+  //   if(err){
+  //     console.error(err);
+  //   }
+  //
+  //   var username = result.username;
+  //   var email = result.email;
+  //   var watched_id_string = result.watched_product_ids;
+  //   var watched_ids = watched_id_string.split(",");
+  //   console.log(watched_ids);
+  //   var query = `SELECT prod_name, prod_price_history, date_update_history FROM productData where prod_id =?`;
+  //   console.log(query);
+  //   db.get(query, [watched_ids], function(error, rows) {
+  //     if(error){
+  //       console.error(err);
+  //     }
+  //     console.log("rows");
+  //     console.log(rows);
+  //   })
+  //   res.render('profile', { title: 'YOUR PROFILE', userid: userid, username: username, email:email});
+  //
+  // });
 });
 
 
@@ -129,7 +199,7 @@ router.post('/sell', function(req,res,next){
 
     const sqlite3 = require('sqlite3').verbose();
     let db = new sqlite3.Database('./db/prices.db');
-  
+
     db.run('CREATE TABLE productData(prod_id TEXT, prod_name TEXT,prod_currency TEXT, prod_price REAL, prod_url TEXT, user_id INTEGER)');// --- INITIAL PRODUCT TABLE HAS BEEN MADE
 
     var userid = req.user.user_id;
@@ -160,7 +230,7 @@ router.post('/sell', function(req,res,next){
     //     price_num = Number(price);
     //     console.log(price_num);
     //   });
-  
+
     //   $('span#productTitle').each(function(i, element) {
     //     var el = $(this);
     //     title_text = el.text();
@@ -168,11 +238,11 @@ router.post('/sell', function(req,res,next){
 
     // is this in the database
     // get id from link
-    // check if its in database 
-      //if is output link to user 
+    // check if its in database
+      //if is output link to user
       //take to home login page with carousel and graph price history
       //need sumbit for them to register buying to store in prfile
-    // else 
+    // else
     db.run(`INSERT INTO productData(prod_name,prod_currency, prod_price, prod_url, user_id) VALUES(?,?,?,?,?)`, [title_text,currency,price_num,url,userid], function(err) {
       if (err) {
         return console.log(err.message);
@@ -180,7 +250,7 @@ router.post('/sell', function(req,res,next){
       console.log('SUCCESS');
       res.redirect('/login');
     });
-  
+
   // if(errors){
   //   req.session.errors = errors;
   //   req.session.success = false;
@@ -221,7 +291,7 @@ function authenticationMiddleware(){
 
     if (req.isAuthenticated()) return next (
       );
-    
+
     res.redirect('/');
   }
 }
